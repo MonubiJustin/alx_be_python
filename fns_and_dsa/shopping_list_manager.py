17b1bb3dada9994d0209aab5074a75a704289887
MonubiJustin <monubijustin@gmail.com>
2025-08-02 04:50:10 +0300

    hehe

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == '1':
             # Prompt for add an item
-            item = input("Enter an item: ").strip()
+            item = input("Enter the item to add: ").strip()
             shopping_list.append(item)
         elif choice == '2':
             # Prompt for and remove an item
