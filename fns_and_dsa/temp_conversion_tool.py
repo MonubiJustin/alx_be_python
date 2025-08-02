c380ab76c001d22e9d8b6102532e7e595410e156
MonubiJustin <monubijustin@gmail.com>
2025-08-02 05:36:02 +0300

    feat: added error handling

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,15 +9,18 @@ def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
-    temperature = float(input("Enter the temperature to convert: "))
-    choice = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+    try:
+        temperature = float(input("Enter the temperature to convert: "))
+        choice = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
 
-    if choice == 'C':
-        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
-    elif choice == 'F':
-        print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
-    else:
-        print("Invalid choice. Please enter 'C' or 'F'.")
+        if choice == 'C':
+            print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
+        elif choice == 'F':
+            print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
+        else:
+            print("Invalid choice. Please enter 'C' or 'F'.")
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value")
 
 if __name__ == '__main__':
     main()
