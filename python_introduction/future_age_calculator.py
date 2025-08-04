1fc5829176e9e655caf848dd3a6377f4cb4fd4e6
MonubiJustin <monubijustin@gmail.com>
2025-08-04 04:17:15 +0300

    deleted a bunch of files

--- python_introduction/future_age_calculator.py
+++
@@ -1,6 +0,0 @@
-age = int(input("How old are you? "))
-current_year = 2023
-future_year = 2050
-new_age = age + (future_year - current_year)
-
-print(f"In {future_year}, you will be {new_age} years old.")
---
+++ test.py
@@ -0,0 +1,7 @@
+a = 9
+b = 0
+
+try:
+    print(a/b)
+except NameError:
+    print("Cannot divide by zero dog")
